1936bcdf74a3fffcc357a78eb296fd1c8b3b7f8c
“Diana <dkomar@umich.edu>
2021-12-16 17:26:40 -0500

    commiting SelectionSort.java and testSelectionSort.java

---
+++ SelectionSort.java
@@ -0,0 +1,45 @@
+package Sort;
+
+public class SelectionSort {
+
+    /* Selection Sort function */
+
+	 public static void sort( int arr[] ){
+
+	        int n = arr.length;
+
+	        int i, j, pos, temp;
+
+	        for (i = 0; i < n-1; i++)
+
+	        {
+
+	            pos = i;
+
+	            for (j = i+1; j < n; j++)
+
+	            {
+
+	                if (arr[j] < arr[pos])
+
+	                {
+
+	                    pos = j;
+
+	                }
+
+	            }
+
+	           
+
+	            temp = arr[pos];
+
+	            arr[pos] = arr[i];
+
+	            arr[i] = temp;
+	            
+	        }
+
+	      }
+
+}
